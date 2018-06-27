2784022c7622f82382cb3fca817cc8b60beafb76
yoshiki <gariyoshi0630@gmail.com>
2018-06-28 02:00:41 +0900

    Add item to take.

--- U16procon_osc2018/Item.cs
+++ U16procon_osc2018/Item.cs
@@ -2,7 +2,20 @@ namespace U16procon {
   namespace Item {
     class Item {
 
-
+      /*
+        周囲のアイテムがあれば取る
+        注：優先度は上>右>左>下
+        data:   周囲情報
+        return: 行動コード
+                周囲にアイテムがない場合は-1を返却します
+       */
+      public int GetItem(int[] data) {
+        if (data[1] == (int)Encode.Field.Item) return (int)Encode.Action_encode.Walkup();
+        else if (data[5] == (int)Encode.Field.Item) return (int)Encode.Action_encode.Walkright();
+        else if (data[3] == (int)Encode.Field.Item) return (int)Encode.Action_encode.Walkleft();
+        else if (data[7] == (int)Encode.Field.Item) return (int)Encode.Action_encode.Walkdown();
+        else return -1;
+      }
     }
   }
 }
